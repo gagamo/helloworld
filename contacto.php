18fb7704c26fad29c41a723add178eb3e0a11553
gagamo <gabriela.garcia@cdhdf.org.mx>
2016-06-01 23:05:06 +0000

    Add files via upload

---
+++ contacto.php
@@ -0,0 +1,41 @@
+<?php include('top.php')?>
+<div class="grid_12 alpha omega">
+<img src="images/contacto.jpg">
+</div>
+<div class="clear clearfix" style="height:20px;"></div>
+<div class="grid_4 alpha">
+<div class="sidebar-img sidebar-abc"> 
+  <img src="images/sidebar-abc.png" />
+    <div class="mask"><a href="abc.php" class="link">&nbsp;</a></div>
+  </div>
+  <div class="sidebar-img sidebar-daa"> 
+  <img src="images/sidebar-daa.png" />
+    <div class="mask"><a href="daa.php" class="link"> &nbsp; </a></div>
+  </div>
+ <div class="sidebar-img sidebar-dnna"> 
+  <img src="images/sidebar-dnna.png" />
+    <div class="mask"><a href="dnna.php" class="link"> &nbsp;</a> </div>
+  </div>
+  <div class="sidebar-img sidebar-efdh"> 
+  <img src="images/sidebar-efdh.png" />
+    <div class="mask"><a href="efdh.php" class="link"> &nbsp; </a></div>
+  </div>
+  <div class="sidebar-img sidebar-gdhmi"> 
+  <img src="images/sidebar-gdhmi.png" />
+    <div class="mask"><a href="gdhmi.php" class="link"> &nbsp; </a></div>
+  </div>
+  <div class="sidebar-img sidebar-gli"> 
+  <img src="images/sidebar-gli.png" />
+    <div class="mask"><a href="gli.php" class="link"> &nbsp;</a> </div>
+  </div>
+  <div class="sidebar-img sidebar-spdh"> 
+  <img src="images/sidebar-spdh.png" />
+    <div class="mask"><a href="spdh.php" class="link"> &nbsp;</a> </div>
+  </div>
+
+</div>
+<div class="grid_8 omega">
+  <p class="titulo">Contacto</p>
+  <p>Si deseas información o realizar la inscripción de un grupo, Llámanos al 52-29-56-00 exensión 1835 de lunes a viernes de 09:00 a 18:00 hrs. envianos un correo electrónico a informes.aprendedh@cdhdf.org.mx</p>
+</div>
+<?php include('footer.php')?>
